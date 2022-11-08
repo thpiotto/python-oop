bb0f8fb9e44102a024d553edf60aae77654061c7
Thales H. P. de Lira <thplira@gmail.com>
2022-11-07 21:29:56 -0300

    script to initialize e test the classes, nothing to care

--- script_bank_initialize.py
+++ script_bank_initialize.py
@@ -1,16 +1,29 @@
 from account import Account
+from client import Client
 
-acc1 = Account(1, 'Thales Lira', 0)
-acc2 = Account(2, "Maria Vitória", 0)
-acc3 = Account(3, "Cássia Regina", 0)
 
-acc1.statement()
-acc2.statement()
-acc3.statement()
+## Inicialization for tests
+
+client1 = Client("Thales Heron", "Piotto de Lira")
+client2 = Client("Maria Vitória", "Reiter Grandi")
+client3 = Client("Cássia Regina", "Piotto de Lira")
+
+acc1 = Account(1, client1, 0)
+acc2 = Account(2, client2, 0)
+acc3 = Account(3, client3, 0)
+
+#acc1.general_info()
+#acc2.general_info()
+#acc3.general_info()
 
 acc1.deposit(500)
-acc2.deposit(1356.9)
-acc3.deposit(-100)
+#acc2.deposit(600)
+#acc3.deposit(-100)
 
-acc1.withdraw(502)
-acc2.withdraw(466.1)
+acc1.withdraw(501)
+acc1.general_info()
+#acc2.withdraw(466.1)
+
+#acc1.transfer(200, acc2)
+#acc1.statement()
+#acc2.statement()
